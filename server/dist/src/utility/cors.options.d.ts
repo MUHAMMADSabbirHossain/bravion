d36fb459d6dcac1ce6cd13aaaa40ee0684d20bbe
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-12-02 11:21:55 +0600

    chore: stop tracking dist folder

--- server/dist/src/utility/cors.options.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const corsOptions: {
-    origin: string;
-    methods: string[];
-    credentials: boolean;
-};
-//# sourceMappingURL=cors.options.d.ts.map
--- server/dist/src/utility/cors.options.js
+++
@@ -1,7 +0,0 @@
-export const corsOptions = {
-    origin: "http://localhost:3000", // Adjust according frontend URL before deployment
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-    // allowedHeaders: ["Content-Type", "Authorization"],
-};
-//# sourceMappingURL=cors.options.js.map
