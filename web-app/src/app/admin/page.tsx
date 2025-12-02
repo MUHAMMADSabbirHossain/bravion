756fae993672f94f981ac9a2b5f02d2b2a1a865f
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-12-02 14:48:53 +0600

    refactor: simplify AdminPage by removing user creation logic and enhancing session validation.

--- web-app/src/app/admin/page.tsx
+++ web-app/src/app/admin/page.tsx
@@ -1,31 +1,29 @@
-"use client";
+import { authClient } from "@/lib/auth-client";
+import { headers } from "next/headers";
+import { forbidden, unauthorized } from "next/navigation";
 
-function AdminPage() {
-  async function handleCreateUser() {
-    const res = await fetch(`http://localhost:5000/admin/create-user`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: "three@test.com",
-        password: "@Md12345",
-        name: "Three Test",
-        role: "user",
-      }),
-    });
-    const data = await res.json();
-    console.log(data);
+async function AdminPage() {
+  const { data: session, error } = await authClient.getSession({
+    fetchOptions: {
+      headers: { cookie: (await headers()).get("cookie") || "" },
+    },
+  });
+  console.log({ session, error });
+
+  if (session === null || !session?.user?.id || error !== null) {
+    return unauthorized();
   }
 
-  return (
-    <>
-      AdminPage
-      <section>
-        <button onClick={handleCreateUser}>Create User</button>
-      </section>
-    </>
-  );
+  if (
+    session === null ||
+    !session?.user?.id ||
+    session?.user?.role !== "admin" ||
+    error !== null
+  ) {
+    return forbidden();
+  }
+
+  return <>AdminPage</>;
 }
 
 export default AdminPage;
