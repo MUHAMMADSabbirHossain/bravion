733f1d0c15cbcd4ff2ffa07492f3a69675b1de61
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-12-02 16:31:23 +0600

    feat: add ProfilePage component for user profile display and session validation.

---
+++ web-app/src/app/profile/page.tsx
@@ -0,0 +1,30 @@
+import { betterAuthGetSeesion } from "@/lib/auth-client";
+import { headers } from "next/headers";
+import Link from "next/link";
+import { unauthorized } from "next/navigation";
+
+async function ProfilePage() {
+  const { data: session, error } = await betterAuthGetSeesion({
+    cookie: (await headers()).get("cookie") || "",
+  });
+  //   console.log({session, error});
+
+  if (session === null || !session?.user?.id || error !== null) {
+    return unauthorized();
+  }
+
+  return (
+    <>
+      ProfilePage
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+      {session?.user?.emailVerified === true ? (
+        <p>Verified</p>
+      ) : (
+        <Link href={""}>Please verify your account.</Link>
+      )}
+    </>
+  );
+}
+
+export default ProfilePage;
