ebbb8d2cc6a4b51a59a588061643e2301d71fd7f
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-12-06 00:05:01 +0600

    feat: add DashboardPage and ProtectedLayout components with session validation.

--- web-app/src/app/(protected)/profile/page.tsx
+++ web-app/src/app/(protected)/profile/page.tsx
@@ -7,11 +7,7 @@ async function ProfilePage() {
   const { data: session, error } = await betterAuthGetSeesion({
     cookie: (await headers()).get("cookie") || "",
   });
-  //   console.log({session, error});
-
-  if (session === null || !session?.user?.id || error !== null) {
-    return unauthorized();
-  }
+  // console.log({ session, error });
 
   return (
     <>
